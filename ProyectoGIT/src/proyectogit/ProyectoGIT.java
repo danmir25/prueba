67cc86a21d8464d65944244796f27edea08c3c01
danmir25 <maxidani9@gmail.com>
2020-02-06 13:11:11 +0100

    Mi primera version
    Que alegria aunque no hay mucho de momento
    
    Signed-off-by: danmir25 <maxidani9@gmail.com>

---
+++ ProyectoGIT/src/proyectogit/ProyectoGIT.java
@@ -0,0 +1,10 @@
+package proyectogit;
+
+public class ProyectoGIT {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Hola mundo");
+	}
+
+}
