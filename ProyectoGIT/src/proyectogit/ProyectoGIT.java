98a74232110e0109ab7baf5e68dc1d7f3d5ab7a5
danmir25 <maxidani9@gmail.com>
2020-02-06 13:15:23 +0100

    Nueva version
    Ahora mola mas!
    
    Signed-off-by: danmir25 <maxidani9@gmail.com>

--- ProyectoGIT/src/proyectogit/ProyectoGIT.java
+++ ProyectoGIT/src/proyectogit/ProyectoGIT.java
@@ -4,7 +4,10 @@ public class ProyectoGIT {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
-		System.out.println("Hola mundo");
+		System.out.println("Hola");
+		System.out.println("Mundo");
+		System.out.println("Que tal");
+
 	}
 
 }
