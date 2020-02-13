3ca70af2d09c1d49df700ad8f4a3fe3466fea37a
danmir25 <60738756+danmir25@users.noreply.github.com>
2020-02-13 13:38:58 +0100

    Update NotasAlumno.java

--- NotasAlumno/src/notasalumno/NotasAlumno.java
+++ NotasAlumno/src/notasalumno/NotasAlumno.java
@@ -1,8 +1,10 @@
 package notasalumno;
-
+/* Programa notas alumno
+ * Version 1.1*/
 public class NotasAlumno {
 	
 	class Alumno{
+		int alumno;
 		int examen1;
 		int examen2;
 		int examen3;
@@ -12,6 +14,20 @@ public class NotasAlumno {
 		int aciertos;
 		int fallos;
 	}
+	public void examenesClasicos() { /* Aqui introduces las notas de los examenes clasicos*/
+      int notaExamenesClasicos=0;
+	  for(int alumno = 1;alumno<=10;alumno++){ 
+		  if (notaExamenesClasicos<=10) {
+			  System.out.println("Tu nota es de " + notaExamenesClasicos);
+		  }else {
+			  System.out.println("Nota no valida");
+			  return;
+		}
+	  }
+    }
+	public void examenesTest(){
+		int notaExamenesTest=0;
+		
+	}
 
 }
-hola
