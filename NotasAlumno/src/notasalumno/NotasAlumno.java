a55719f2abbbe489434e455ee47343ed0df00b54
danmir25 <60738756+danmir25@users.noreply.github.com>
2020-02-13 21:38:36 +0100

    Update NotasAlumno.java

--- NotasAlumno/src/notasalumno/NotasAlumno.java
+++ NotasAlumno/src/notasalumno/NotasAlumno.java
@@ -1,8 +1,9 @@
 package notasalumno;
 /* Programa notas alumno
- * Version 1.1*/
+ * Version 1.2*/
+import java.util.Scanner;
 public class NotasAlumno {
-	
+	Scanner stdin = new Scanner (System.in);
 	class Alumno{
 		int alumno;
 		int examen1;
@@ -14,6 +15,22 @@ public class NotasAlumno {
 		int aciertos;
 		int fallos;
 	}
+        public void examenes(){
+     
+            System.out.println("Inserta la nota del primer examen: ");
+                    int examen1 =stdin.nextInt();
+            System.out.println("Inserta la nota del segundo examen: ");
+                    int examen2 =stdin.nextInt();
+             System.out.println("Inserta la nota del tercer examen: ");
+                    int examen3 =stdin.nextInt();
+             System.out.println("Inserta la nota del cuarto examen: ");
+                    int examen4 =stdin.nextInt();
+             System.out.println("Inserta la nota del quinto examen: ");
+                    int examen5 =stdin.nextInt();
+             int notaFinal = (int) (examen1*0.1 + examen2*0.2 + examen3*0.2 + examen4*0.25 + examen5*025);
+             System.out.println(notaFinal);
+        }
+        
 	public void examenesClasicos() { /* Aqui introduces las notas de los examenes clasicos*/
       int notaExamenesClasicos=0;
 	  for(int alumno = 1;alumno<=10;alumno++){ 
@@ -29,5 +46,8 @@ public class NotasAlumno {
 		int notaExamenesTest=0;
 		
 	}
+        public static void main(String[] args) {
+        // TODO code application logic here
+    }
 
 }
