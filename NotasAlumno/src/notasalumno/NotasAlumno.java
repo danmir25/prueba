8fe83c818ca17ccba93e1b0563b152eabf3d8740
danmir25 <60738756+danmir25@users.noreply.github.com>
2020-02-13 12:22:32 +0100

    Add files via upload

---
+++ NotasAlumno/src/notasalumno/NotasAlumno.java
@@ -0,0 +1,16 @@
+package notasalumno;
+
+public class NotasAlumno {
+	
+	class Alumno{
+		int examen1;
+		int examen2;
+		int examen3;
+		int examen4;
+		int examen5;
+		int respuestas;
+		int aciertos;
+		int fallos;
+	}
+
+}
