1d1dfeac5018055be4e2f771b272e5974dc5e8b9
danmir25 <60738756+danmir25@users.noreply.github.com>
2020-03-05 13:24:44 +0100

    Update NotasAlumno.java

--- NotasAlumno/src/notasalumno/NotasAlumno.java
+++ NotasAlumno/src/notasalumno/NotasAlumno.java
@@ -1,13 +1,12 @@
 package notasalumno;
 /* Programa notas alumno
- * Version 2.3.1*/
+ * Version 3.1*/
 /*Programa para calcular la nota media de 10 alumnos*/
 import java.util.Scanner;
 public class NotasAlumno {
 	Scanner stdin = new Scanner (System.in);
 
 	 private static int alumnos;
-	 int trabajos = 0;
 	 double sumaExamenesClasicos=0;
 	 double sumaExamenesTest=0;
 	 double notaFinal=0;
@@ -53,8 +52,8 @@ public class NotasAlumno {
 			System.out.println("EXAMEN TIPO TEST " + examenesTest);
 			System.out.println("Insterta tus aciertos: ");
 				aciertos=stdin.nextInt();
-				if(aciertos>30) {
-					System.out.println("ERROR. No hay tantas preguntas");
+				if(aciertos>30||aciertos<0) {
+					System.out.println("ERROR. Pon un numero entre 0 y 30");
 						sumaExamenesTest=0;
 						return calcularExamenesTest();
 				}
@@ -75,6 +74,9 @@ public class NotasAlumno {
 				}
 				System.out.println();
 				notaExamenesTest=(aciertos-puntosPerdidos)/3;
+					if(notaExamenesTest<0) {
+						notaExamenesTest=0;
+					}
 				System.out.println("Tu nota en el examen es " + notaExamenesTest);
                         sumaExamenesTest=sumaExamenesTest + notaExamenesTest;
                         System.out.println("Suma total de tus examenes test " + sumaExamenesTest);
@@ -85,50 +87,55 @@ public class NotasAlumno {
 	
 	
         public void presentarTrabajos(){//Para saber si has entregado todos los trabajos.
-          
-            System.out.println("Introduce tus trabajos entregados: ");
-                     trabajos = stdin.nextInt();
-			if (trabajos>=3){  //Calcula si lo has entragado todo.				   
-                  System.out.println("Lo has entregado todo.");
-                  notaFinal = (sumaExamenesClasicos/3)*0.5 + (sumaExamenesTest/2)*0.5;
-                  System.out.println("Tu nota es: " + notaFinal);
-              } else { /*Si no has entragado todo te ponen la nota por defecto*/
-            	  notaFinal = 3;
-                  System.out.println("Has suspendido. Tu nota es de " + notaFinal);
-                  
+            int dias;
+            int trabajos;
+            int confirmacionTrabajos;
+            for(trabajos=1;trabajos<=3;trabajos++){ //Te pregunta por los tres trabajos
+            notaFinal = (sumaExamenesClasicos/3)*0.5 + (sumaExamenesTest/2)*0.5;
+            System.out.println("Tu nota es: " + notaFinal);
+        		 System.out.println("Introduce si has entregado el trabajo " + (trabajos) +" (Nº Positivo para si, 0 o negativo para no): ");
+                 confirmacionTrabajos = stdin.nextInt();
+              if (trabajos>0){  //Calcula si lo has entragado todo.				   
+            	  System.out.println("Entregado.");
+            	  System.out.println("Insterta los dias de retraso: ");
+          			dias=stdin.nextInt();
+          			if(dias>5) {
+          				System.out.println("Trabajo no entregado");
+          				notaFinal=3;
+          				System.out.println("Has suspendido. Tu nota final es de " + notaFinal);
+          				sumaExamenesClasicos=0;
+            			sumaExamenesTest=0;
+            			break;
+          			}else{		
+                		notaFinal=notaFinal-(dias*0.1);
+                		System.out.println("La nota final actual del alumno " + alumnos + " es " + notaFinal);
+          			}
+              }else{
+            	  System.out.println("No entregado");
+      			  notaFinal=3;
+            	  System.out.println("Tu nota es: " + notaFinal);
+            	  sumaExamenesClasicos=0;
+      			  sumaExamenesTest=0;
+      			  break;
               }
-        }
-        
-        
-        public void calcularRetrasoTrabajos() { //Penalizacion si hay retraso en una entraga de un trabajo
-        	int dias;
-        	System.out.println("Insterta los dias de retraso: ");
-        		dias=stdin.nextInt();
-        	if(dias>5) {
-        		System.out.println("Trabajo no entregado");
-        		notaFinal=3;
-                System.out.println("Has suspendido. Tu nota final es de " + notaFinal);
-        	}else{
-        		notaFinal=notaFinal-(dias*0.1);
-        		System.out.println("La nota final del alumno " + alumnos + " es " + notaFinal);
-        			
+              
         	}
-        	sumaExamenesClasicos=0;
-			sumaExamenesTest=0;
-			notaFinal=0;
+        	System.out.println("La nota final del alumno " + alumnos + " es " + notaFinal);
+			
+    
         }
+         
+        
         
         public static void main(String[] args) {
 			// TODO code application logic here
         	NotasAlumno Alumno = new NotasAlumno();
-                System.out.println("¡Bienvenido!");
                 System.out.println("Este es un programa para calcular las notas de 10 alumnos. Por favor, insterte las notas a continuación");
          	for(alumnos = 1; alumnos <= 10; alumnos++){
          		System.out.println("ALUMNO " + alumnos);
         		Alumno.calcularExamenesClasicos();
         		Alumno.calcularExamenesTest();
         		Alumno.presentarTrabajos();
-        		Alumno.calcularRetrasoTrabajos();
         	}
 
         }
